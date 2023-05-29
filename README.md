ecf4cd24aac5a6d1de61c28d4de25cec8de525fd
Elijah Okello <elijahokello90@gmail.com>
2023-05-29 15:58:37 +0300

    added tree cutting inferencing library

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # edge-device
+
+This is arduino code for a device that runs a machine learning model to recognise tree cutting activities in a forest.
+
+Here is a list of sounds that were used to train a model for this usecase.
--- main/main.ino
+++ main/main.ino
@@ -1,88 +1,240 @@
-#include <WiFiNINA.h>
+/* Edge Impulse ingestion SDK
+ * Copyright (c) 2022 EdgeImpulse Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// If your target is limited in memory remove this macro to save 10K RAM
+#define EIDSP_QUANTIZE_FILTERBANK   0
+
+/*
+ ** NOTE: If you run into TFLite arena allocation issue.
+ **
+ ** This may be due to may dynamic memory fragmentation.
+ ** Try defining "-DEI_CLASSIFIER_ALLOCATION_STATIC" in boards.local.txt (create
+ ** if it doesn't exist) and copy this file to
+ ** `<ARDUINO_CORE_INSTALL_PATH>/arduino/hardware/<mbed_core>/<core_version>/`.
+ **
+ ** See
+ ** (https://support.arduino.cc/hc/en-us/articles/360012076960-Where-are-the-installed-cores-located-)
+ ** to find where Arduino installs cores on your machine.
+ **
+ ** If the problem persists then there's not enough memory for this model and application.
+ */
+
+/* Includes ---------------------------------------------------------------- */
+#include <treeCuttingDetection_inferencing.h>
 #include <PDM.h>
 
-bool LED_SWITCH = false;
+/** Audio buffers, pointers and selectors */
+typedef struct {
+    int16_t *buffer;
+    uint8_t buf_ready;
+    uint32_t buf_count;
+    uint32_t n_samples;
+} inference_t;
 
-// default number of output channels
-static const char channels = 1;
-
-// default PCM output frequency
-static const int frequency = 16000;
-
-// Buffer to read samples into, each sample is 16-bits
-short sampleBuffer[512];
-
-// Number of audio samples read
-volatile int samplesRead;
-
-void setup(){
-    Serial.begin(9600);
-    pinMode(LEDB, OUTPUT);
-    while(!Serial);
-    // Configure the data receive callback
-    PDM.onReceive(onPDMdata);
-
-    
-    // Optionally set the gain
-    // Defaults to 20 on the BLE Sense and -10 on the Portenta Vision Shields
-    // PDM.setGain(30);
-
-    // Initialize PDM with:
-    // - one channel (mono mode)
-    // - a 16 kHz sample rate for the Arduino Nano 33 BLE Sense
-    // - a 32 kHz or 64 kHz sample rate for the Arduino Portenta Vision Shields
-    if(!PDM.begin(channels, frequency)){
-        Serial.println("Failed to start PDM! ");
-        while(1);
-    }
-}
-
-void loop(){
-    // Wait for samples to be read
-    if(samplesRead){
-        for(int i = 0; i < samplesRead; i++){
-            if(channels == 2){
-              Serial.println("L:");
-              Serial.print(sampleBuffer[i]);
-              Serial.print(" R:");
-            }
-            Serial.println(sampleBuffer[i]);
-            if(sampleBuffer[i] > 10000 || sampleBuffer[i] < -10000){
-              LED_SWITCH = !LED_SWITCH;
-              if(LED_SWITCH){
-                Serial.println();
-                digitalWrite(LEDB,HIGH);
-                Serial.println("ON!");
-                Serial.println();
-                delay(1000);
-              }
-              else{
-                Serial.println();
-                digitalWrite(LEDB, LOW);
-                Serial.println("OFF!");
-                Serial.println();
-                delay(1000);
-              }
-            }
-        }
-        // Clear the read count
-        samplesRead = 0;
-    }
-}
-
+static inference_t inference;
+static signed short sampleBuffer[2048];
+static bool debug_nn = false; // Set this to true to see e.g. features generated from the raw signal
+static volatile bool record_ready = false;
 
 /**
-   Callback function to process the data from the PDM microphone.
-   NOTE: This callback is executed as part of an ISR.
-   Therefore using `Serial` to print messages inside this function isn't supported.
- * */
-void onPDMdata(){
-  // Query the number of available bytes
-  int bytesAvaliable = PDM.available();
+ * @brief      Arduino setup function
+ */
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    // comment out the below line to cancel the wait for USB connection (needed for native USB)
+    while (!Serial);
+    Serial.println("Edge Impulse Inferencing Demo");
 
-  // Read into the sample buffer
-  PDM.read(sampleBuffer, bytesAvaliable);
+    // summary of inferencing settings (from model_metadata.h)
+    ei_printf("Inferencing settings:\n");
+    ei_printf("\tInterval: ");
+    ei_printf_float((float)EI_CLASSIFIER_INTERVAL_MS);
+    ei_printf(" ms.\n");
+    ei_printf("\tFrame size: %d\n", EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+    ei_printf("\tSample length: %d ms.\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT / 16);
+    ei_printf("\tNo. of classes: %d\n", sizeof(ei_classifier_inferencing_categories) / sizeof(ei_classifier_inferencing_categories[0]));
 
-  // 16bit, 2 bytes per sample
-  samplesRead = bytesAvaliable / 2;
+    if (microphone_inference_start(EI_CLASSIFIER_RAW_SAMPLE_COUNT) == false) {
+        ei_printf("ERR: Could not allocate audio buffer (size %d), this could be due to the window length of your model\r\n", EI_CLASSIFIER_RAW_SAMPLE_COUNT);
+        return;
+    }
 }
+
+/**
+ * @brief      Arduino main function. Runs the inferencing loop.
+ */
+void loop()
+{
+    ei_printf("Starting inferencing in 2 seconds...\n");
+
+    delay(2000);
+
+    ei_printf("Recording...\n");
+
+    bool m = microphone_inference_record();
+    if (!m) {
+        ei_printf("ERR: Failed to record audio...\n");
+        return;
+    }
+
+    ei_printf("Recording done\n");
+
+    signal_t signal;
+    signal.total_length = EI_CLASSIFIER_RAW_SAMPLE_COUNT;
+    signal.get_data = &microphone_audio_signal_get_data;
+    ei_impulse_result_t result = { 0 };
+
+    EI_IMPULSE_ERROR res = run_classifier_continuous(&signal, &result, debug_nn);
+    if (res != EI_IMPULSE_OK) {
+        ei_printf("ERR: Failed to run classifier (%d)\n", res);
+        return;
+    }
+
+    // print inference return code
+    ei_printf("run_classifier returned: %d\r\n", res);
+    print_inference_result(result);
+}
+
+/**
+ * @brief      PDM buffer full callback
+ *             Copy audio data to app buffers
+ */
+static void pdm_data_ready_inference_callback(void)
+{
+    int bytesAvailable = PDM.available();
+
+    // read into the sample buffer
+    int bytesRead = PDM.read((char *)&sampleBuffer[0], bytesAvailable);
+
+    if ((inference.buf_ready == 0) && (record_ready == true)) {
+
+        for(int i = 0; i < bytesRead>>1; i++) {
+            inference.buffer[inference.buf_count++] = sampleBuffer[i];
+
+            if(inference.buf_count >= inference.n_samples) {
+                inference.buf_count = 0;
+                inference.buf_ready = 1;
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * @brief      Init inferencing struct and setup/start PDM
+ *
+ * @param[in]  n_samples  The n samples
+ *
+ * @return     { description_of_the_return_value }
+ */
+static bool microphone_inference_start(uint32_t n_samples)
+{
+    inference.buffer = (int16_t *)malloc(n_samples * sizeof(int16_t));
+
+    if(inference.buffer == NULL) {
+        return false;
+    }
+
+    inference.buf_count  = 0;
+    inference.n_samples  = n_samples;
+    inference.buf_ready  = 0;
+
+    // configure the data receive callback
+    PDM.onReceive(pdm_data_ready_inference_callback);
+
+    PDM.setBufferSize(2048);
+    delay(250);
+
+    // initialize PDM with:
+    // - one channel (mono mode)
+    if (!PDM.begin(1, EI_CLASSIFIER_FREQUENCY)) {
+        ei_printf("ERR: Failed to start PDM!");
+        microphone_inference_end();
+        return false;
+    }
+
+    // optionally set the gain, defaults to 24
+    // Note: values >=52 not supported
+    //PDM.setGain(40);
+
+    return true;
+}
+
+/**
+ * @brief      Wait on new data
+ *
+ * @return     True when finished
+ */
+static bool microphone_inference_record(void)
+{
+    bool ret = true;
+
+    record_ready = true;
+    while (inference.buf_ready == 0) {
+        delay(10);
+    }
+
+    inference.buf_ready = 0;
+    record_ready = false;
+
+    return ret;
+}
+
+/**
+ * Get raw audio signal data
+ */
+static int microphone_audio_signal_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    numpy::int16_to_float(&inference.buffer[offset], out_ptr, length);
+
+    return 0;
+}
+
+/**
+ * @brief      Stop PDM and release buffers
+ */
+static void microphone_inference_end(void)
+{
+    PDM.end();
+    ei_free(inference.buffer);
+}
+
+void print_inference_result(ei_impulse_result_t result) {
+
+    // Print how long it took to perform inference
+    ei_printf("Timing: DSP %d ms, inference %d ms, anomaly %d ms\r\n",
+            result.timing.dsp,
+            result.timing.classification,
+            result.timing.anomaly);
+
+    ei_printf("Predictions:\r\n");
+    for (uint16_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
+        ei_printf("  %s: ", ei_classifier_inferencing_categories[i]);
+        ei_printf("%.5f\r\n", result.classification[i].value);
+    }
+
+    // Print anomaly result (if it exists)
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+    ei_printf("Anomaly prediction: %.3f\r\n", result.anomaly);
+#endif
+
+}
+
+#if !defined(EI_CLASSIFIER_SENSOR) || EI_CLASSIFIER_SENSOR != EI_CLASSIFIER_SENSOR_MICROPHONE
+#error "Invalid model for current sensor."
+#endif
